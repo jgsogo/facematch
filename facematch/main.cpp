54254b8d820b453c48716a43c5df82c605729c46
Javier G. Sogo <jgsogo@gmail.com>
2015-05-11 21:19:34 +0200

    skeleton

--- facematch/main.cpp
+++ facematch/main.cpp
@@ -3,25 +3,21 @@
 
 using namespace cv;
 
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+int main(int argc, char** argv ) {
+    if ( argc != 2 ) {
+        printf("usage: facematch <path/to/images>\n");
         return -1;
     }
 
-    Mat image;
-    image = imread( argv[1], 1 );
+    // Read images and build curated dataset:
+    //  * read all images in the input folder and subfolders recursively
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    //  * work on each image to get faces
+    //  * crop and rescale
 
+    //  * apply unsupervised classification: kmeans
+    //  * store each cluster into a different folder and name the photos with numbers
+    //  proportional to the distance to the centroid.
     waitKey(0);
 
     return 0;
