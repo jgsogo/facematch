a1519ea0fdda14b12e2f947d8f863b98a3ae87d2
Javier G. Sogo <jgsogo@gmail.com>
2015-05-18 11:57:36 +0200

    create also a readable labels file

--- utils/build_csv.py
+++ utils/build_csv.py
@@ -8,17 +8,24 @@ def build_csv(directory, output):
     label = 0
     filename = os.path.basename(directory)
     filename = os.path.join(output, '%s.csv' % filename)
+    labels = os.path.join(output, 'labels.csv')
+
     f = open(filename, 'w')
-    i = 0
+    flabels = open(labels, 'w')
     f.write('#id;path;label\n')
+    f.write('#id;label\n')
+
+    i = 0
     for root, dirs, files in os.walk(directory):
         for name in files:
             if name.lower().endswith(('png', 'jpg', 'jpeg')):
                 path = os.path.abspath(os.path.join(root, name))
                 f.write('%d;%s;%d\n' % (i, path, label))
                 i += 1
+        flabels.write('%d;%s\n' % (label, os.path.basename(root)))
         label += 1
     f.close()
+    flabels.close()
 
               
 if __name__ == '__main__':
